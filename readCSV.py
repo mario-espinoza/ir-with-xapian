3eab568a18479349a8ca5db42d1e187222d16cff
mespinozas <mario.espinoza@gmail.com>
2016-11-15 19:37:28 -0300

    make and or queries

--- readCSV.py
+++ readCSV.py
@@ -13,37 +13,32 @@ classes={
 	'7':'Complejas',
 	'8':'Web y Archivada'
 }
-	
-with open('newMarioEspinoza.csv', 'w+') as ocsvfile:
-	with open('marioEspinoza.csv', 'rU') as icsvfile:
-		spamwriter = csv.writer(ocsvfile, delimiter=',')
-		spamreader = csv.reader(icsvfile, delimiter=',')
-		spamwriter.writerow(['ID','TITLE','CLASS','BODY'])
-		for i,row in enumerate(spamreader):	
-			if i>0:
-				idText = row[0].replace('\n', ' ').strip();
-				tag = row[1].replace('\n', ' ').strip();
-				title = row[2].replace('\n', ' ').strip();
-				body=''
-				# try:
-				body = row[3]
-				# .replace('\n', ' ').strip();
-				body = ' '.join(x.strip() for x in body.split())
-					# text = title + ' ' +body
-				# except IndexError:		
-					# text = title
-				
 
-				if len(body) == 0:
-					noBody=noBody+1;
-					print 'Empty'
-				else: 
-										
-					withBody=withBody+1;
-					print '{}'.format(idText);
-					print '{}'.format(title);
-					print 'len: {}'.format(len(body));
-					print 'body:"{}"'.format(body);
-					spamwriter.writerow([idText,title,classes[tag],body])
+with open('titles.txt', 'w+') as titleFile:	
+	with open('dataWithBody.csv', 'w+') as ocsvfile:
+		with open('marioEspinoza.csv', 'rU') as icsvfile:
+			spamwriter = csv.writer(ocsvfile, delimiter=',')
+			spamreader = csv.reader(icsvfile, delimiter=',')
+			spamwriter.writerow(['ID','TITLE','CLASS','BODY'])
+			for i,row in enumerate(spamreader):	
+				if i>0:
+					idText = row[0].replace('\n', ' ').strip();
+					tag = row[1].replace('\n', ' ').strip();
+					title = row[2].replace('\n', ' ').strip();
+					body = row[3]
+					body = ' '.join(x.strip() for x in body.split())
+
+					if len(body) == 0:
+						noBody=noBody+1;
+						print 'Empty'
+					else: 
+											
+						withBody=withBody+1;
+						print '{}'.format(idText);
+						print '{}'.format(title);
+						print 'len: {}'.format(len(body));
+						print 'body:"{}"'.format(body);
+						spamwriter.writerow([idText,title,classes[tag],body]);
+						titleFile.write('{}\n'.format(title));
 print 'withBody: {}'.format(withBody);
 print 'nobody: {}'.format(noBody);
--- searcher2.py
+++ searcher2.py
@@ -13,16 +13,13 @@ def log_matches(querystring, offset, pagesize, matches):
         )
 
 ### Start of Search.
-def search(dbpath, title, offset=0, pagesize=10):
-    print 'TITLE: {}'.format(title)
+def search(dbpath, title,  offset=0, pagesize=10):
+    print '{}'.format(title)
     regex = re.findall(r'\w+', title.lower())
+    print 'REGEX: {}'.format(regex)
     queryAND = ' AND '.join(regex)
     queryOR = ' OR '.join(regex)
-    print 'REGEX: {}'.format(regex)
-    # search('DB', queryAND.encode('utf-8'))
-    # search('DB', queryOR.encode('utf-8'))
-    queries = [queryAND,queryOR]
-    for querystring in queries:
+    for querystring in [queryAND.encode('utf-8'),queryOR.encode('utf-8')]:
         print 'QUERY: {}'.format(querystring)
         # offset - defines starting point within result set
         # pagesize - defines number of records to retrieve
@@ -51,16 +48,14 @@ def search(dbpath, title, offset=0, pagesize=10):
         # And print out something about each match
         matches = []
         for match in enquire.get_mset(offset, pagesize):
-            toDecode = match.document.get_data();
-            fields = json.loads(toDecode.decode('utf-8', 'ignore'))
-            matchTitle =fields.get('TITLE', u'') 
-            if title == matchTitle:
-                print '\nThis is \n'
-            print(u"%(rank)i: #%(docid)3.3i \n %(id)s \n TITLE: %(title)s \n %(body)s \n %(class)s" % {
+            fields = json.loads(match.document.get_data())
+            # if title == fields.get('TITLE', u''):
+            #     print 'This is \n'
+            print(u"%(rank)i: #%(docid)3.3i \n %(id)s \n %(title)s \n %(body)s \n %(class)s \n" % {
                 'rank': match.rank + 1,
                 'docid': match.docid,
                 'id': fields.get('ID', u''),
-                'title': matchTitle,
+                'title': fields.get('TITLE', u''),
                 'body': fields.get('BODY', u''),
                 'class': fields.get('CLASS', u'')
                 })
@@ -68,13 +63,11 @@ def search(dbpath, title, offset=0, pagesize=10):
 
         # Finally, make sure we log the query and displayed results
         log_matches(querystring, offset, pagesize, matches)
-    print '\n'
 ### End of example code.
 
 logging.basicConfig(level=logging.INFO)
 with open('titles.txt','rU') as titleFile:
     titles = titleFile.readlines()
     for title in titles:
-        print '{}'.format(title)
-        search('DB', title)
+        search('DB', title.encode('utf-8'))
         # search('DB', querystring = " ".join(title))
