b931b9e9a3b2884bc7bbb01ccb9fef7bfe87b19d
mespinozas <mario.espinoza@gmail.com>
2016-11-17 13:57:23 -0300

    data sagal gonzalez

--- readCSV.py
+++ readCSV.py
@@ -1,31 +1,34 @@
-import csv
+import csv,io
 
 withBody = 0;
 noBody = 0;
 
-classes={
-	'1':'Opiniones',
-	'2':'No respondibles',
-	'3':'Particulares',
-	'4':'Otros',
-	'5':'Archivada',
-	'6':'Web',
-	'7':'Complejas',
-	'8':'Web y Archivada'
-}
+# classes={
+# 	'1':'Opiniones',
+# 	'2':'No respondibles',
+# 	'3':'Particulares',
+# 	'4':'Otros',
+# 	'5':'Archivada',
+# 	'6':'Web',
+# 	'7':'Complejas',
+# 	'8':'Web y Archivada'
+# }
 
-with open('titles.txt', 'w+') as titleFile:	
-	with open('dataWithBody.csv', 'w+') as ocsvfile:
-		with open('marioEspinoza.csv', 'rU') as icsvfile:
+with io.open('titlesGonzalez.txt', 'w+', encoding='utf-8') as titleFile:	
+	with open('dataWithBodyGonzalez.csv', 'w+') as ocsvfile:
+		with open('gonzalez.csv', 'rU') as icsvfile:
 			spamwriter = csv.writer(ocsvfile, delimiter=',')
-			spamreader = csv.reader(icsvfile, delimiter=',')
-			spamwriter.writerow(['ID','TITLE','CLASS','BODY'])
+			spamreader = csv.reader(icsvfile, delimiter=';')
+			#id_Pregunta;Titulo;Cuerpo;Categoria;Clase;HWeb;HYA;NResp;Seguidores
+			#spamwriter.writerow(['ID','TITLE','CLASS','BODY'])
+			spamwriter.writerow(['ID','TITLE','BODY','CAT','CLASS'])
 			for i,row in enumerate(spamreader):	
 				if i>0:
 					idText = row[0].replace('\n', ' ').strip();
-					tag = row[1].replace('\n', ' ').strip();
-					title = row[2].replace('\n', ' ').strip();
-					body = row[3]
+					tag = row[4].replace('\n', ' ').strip();
+					title = row[1].replace('\n', ' ').strip();
+					body = row[2]
+					cat = row[3].replace('\n', ' ').strip();
 					body = ' '.join(x.strip() for x in body.split())
 
 					if len(body) == 0:
@@ -38,7 +41,8 @@ with open('titles.txt', 'w+') as titleFile:
 						print '{}'.format(title);
 						print 'len: {}'.format(len(body));
 						print 'body:"{}"'.format(body);
-						spamwriter.writerow([idText,title,classes[tag],body]);
-						titleFile.write('{}\n'.format(title));
+						# spamwriter.writerow([idText,title,classes[tag],body]);
+						spamwriter.writerow([idText,title,body,cat,tag]);
+						titleFile.write(unicode(title+'\n', errors='ignore'));
 print 'withBody: {}'.format(withBody);
 print 'nobody: {}'.format(noBody);
--- searcher.py
+++ searcher.py
@@ -14,20 +14,18 @@ def log_matches(querystring, offset, pagesize, matches):
 
 ### Start of Search.
 def search(dbpath, title, offset=0, pagesize=10):
+    # offset - defines starting point within result set
+    # pagesize - defines number of records to retrieve
+    
     print 'TITLE: {}'.format(title)
     regex = re.findall(r'\w+', title.lower())
     queryAND = ' AND '.join(regex)
     queryOR = ' OR '.join(regex)
     print 'REGEX: {}'.format(regex)
-    # search('DB', queryAND.encode('utf-8'))
-    # search('DB', queryOR.encode('utf-8'))
     queries = [queryAND,queryOR]
+
     for querystring in queries:
         print 'QUERY: {}'.format(querystring)
-        # offset - defines starting point within result set
-        # pagesize - defines number of records to retrieve
-
-        # Open the database we're going to search.
         db = xapian.Database(dbpath)
 
         # Set up a QueryParser with a stemmer and suitable prefixes
@@ -50,22 +48,31 @@ def search(dbpath, title, offset=0, pagesize=10):
 
         # And print out something about each match
         matches = []
+        MRR=0
         for match in enquire.get_mset(offset, pagesize):
             toDecode = match.document.get_data();
             fields = json.loads(toDecode.decode('utf-8', 'ignore'))
-            matchTitle =fields.get('TITLE', u'') 
-            if title == matchTitle:
-                print '\nThis is \n'
-            print(u"%(rank)i: #%(docid)3.3i \n %(id)s \n TITLE: %(title)s \n %(body)s \n %(class)s" % {
-                'rank': match.rank + 1,
-                'docid': match.docid,
-                'id': fields.get('ID', u''),
-                'title': matchTitle,
-                'body': fields.get('BODY', u''),
-                'class': fields.get('CLASS', u'')
-                })
-            matches.append(match.docid)
+            matchTitle = fields.get('TITLE', u'')
+            matchRank=match.rank + 1
+            # if matchRank>1 and title == matchTitle :
+                # print '\n *** \n'
+                # print title
+                # print matchTitle
+                # print 'matchRank: {}\n'.format(matchRank)
+                # # print 'MRR: {}\n'.format(1/matchRank)
+                # print '\n *** \n'
 
+            print(u"%(rank)i: #%(docid)3.3i \n %(id)s \n TITLE: %(title)s \n %(body)s \n %(class)s" % {
+                    'rank': matchRank,
+                    'docid': match.docid,
+                    'id': fields.get('ID', u''),
+                    'title': matchTitle,
+                    'body': fields.get('BODY', u''),
+                    'class': fields.get('CLASS', u'')
+                    })
+            matches.append(match.docid)
+            MRR=1/matchRank
+        print 'MRR: {}\n'.format(MRR)
         # Finally, make sure we log the query and displayed results
         log_matches(querystring, offset, pagesize, matches)
     print '\n'
@@ -76,5 +83,5 @@ with open('titles.txt','rU') as titleFile:
     titles = titleFile.readlines()
     for title in titles:
         print '{}'.format(title)
-        search('DB', title)
+        search('DB', title.strip())
         # search('DB', querystring = " ".join(title))
